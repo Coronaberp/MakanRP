e60a9ced037770e8903d4d317902742e3aa798d9
Jason <jasonmohjs@gmail.com>
2026-01-15 15:06:22 +0800

    Rebuild and update dependencies with latest changes

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
+const base = process.env.PUBLIC_URL || '/MakanRP';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={base}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
